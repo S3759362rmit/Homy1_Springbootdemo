0973380ecd267da72f9ab1f7a7a4b66d755285ce
S3759362rmit <82450159+S3759362rmit@users.noreply.github.com>
2022-08-18 16:55:48 +1000

    test

--- src/main/java/com/homy/rest/SpringBootDemoApplication.java
+++ src/main/java/com/homy/rest/SpringBootDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
+        int i;
+        i = 0;
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 }
